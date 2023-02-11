aba647e2f2a26280ccc922e7c1c806f4966419f1
pardeepky <pardeep.pky@gmail.com>
2023-02-11 15:07:55 +0530

    changed div wrapper to fragments

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './button.css';
 
 const Button = props => {
-    return (
-        <button className='button' type={props.type} onClick={props.onClick}>{props.children}</button>
-    )
+    return <button className='button' type={props.type} onClick={props.onClick}>{props.children}</button>
 }
 
 export default Button
