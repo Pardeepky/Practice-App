3d361b3af12cb78ae09618ff26390d19c8ac0fab
pardeepky <pardeep.pky@gmail.com>
2023-02-11 15:23:48 +0530

    created fortal for modal

--- src/UI/ErrorModal.js
+++ src/UI/ErrorModal.js
@@ -2,22 +2,33 @@ import React from 'react'
 import Card from './Card'
 import './errorModal.css'
 import Button from './Button'
+import ReactDom from 'react-dom'
+
+const Backdrop = props => {
+    return <div className='backdrop' onClick={props.onConfirm} />
+}
+
+const Modal = props => {
+    return (
+        <Card className='modal'>
+            <header className='header'>
+                <h2>{props.title}</h2>
+            </header>
+            <div className='content'>
+                <p>{props.message}</p>
+            </div>
+            <footer className='actions'>
+                <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+        </Card>
+    )
+}
 
 const ErrorModal = props => {
     return (
         <>
-            <div className='backdrop' onClick={props.onConfirm} />
-            <Card className='modal'>
-                <header className='header'>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className='content'>
-                    <p>{props.message}</p>
-                </div>
-                <footer className='actions'>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
+            {ReactDom.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))}
+            {ReactDom.createPortal(<Modal title={props.title} message={props.message} onConfirm={props.onConfirm} />, document.getElementById('modal-root'))}
         </>
     )
 }
